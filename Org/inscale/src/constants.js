c71dd9d277e3893b59c1fcc09f76ec793a7c3d4e
tecrajesh <tecrajesh@gmail.com>
2019-10-14 13:50:41 +0530

    Test coverage

---
+++ Org/inscale/src/constants.js
@@ -0,0 +1,9 @@
+export const metaInfo = {
+  data:
+    [{ "id": 1, "name": "Divavu", "startDate": "9/19/2017", "endDate": "3/9/2020", "Budget": 88377 },
+    { "id": 10, "name": "Realbridge", "startDate": "3/5/2017", "endDate": "10/2/2018", "Budget": 505602111111 }]
+
+  , headers: ["Name", "Start Date", "End Date", "Active", "Budget"],
+  columns: ["name", "startDate", "endDate", "active", "Budget"],
+  dynamicColumns: ["active", "Budget"]
+}
